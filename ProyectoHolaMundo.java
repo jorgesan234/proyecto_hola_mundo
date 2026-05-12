be5a6a2a2e6724749e0844d6e4eeb86651241925
Jorge <jorgesanluis62@gmail.com>
2026-05-12 12:38:19 -0300

    added 2 numbers

--- ProyectoHolaMundo.java
+++ ProyectoHolaMundo.java
@@ -1,5 +1,15 @@
 public class ProyectoHolaMundo{
     public static void main(String[] args) {
         System.out.print("Hola mundo");
+        System.out.println("Bienvenido Java");
+        System.out.println("Y a Git/GitHub");
+
+
+        int numero1 = 10;
+        int numero2 = 15;
+
+        int suma = numero1 + numero2;
+        
+        System.out.println(suma);
     }
 }
