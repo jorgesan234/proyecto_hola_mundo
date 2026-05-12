602e2be595c04d86702ac7cdaac923960d0e88fc
Jorge <jorgesanluis62@gmail.com>
2026-05-12 12:45:21 -0300

    create the class person

---
+++ Persona.java
@@ -0,0 +1,13 @@
+public class Persona {
+
+    private String Nombre;
+    private  int Edad;
+    private String Direccion;
+
+    public Persona (String Nombre,int Edad,String Direccion){
+        this.Nombre = Nombre;
+        this.Edad = Edad;
+        this.Direccion = Direccion;
+    }
+    
+}
